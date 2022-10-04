b4d85fae0783beab927f47342b4889999e3aff46
XiaoPan <2489964425@qq.com>
2022-10-04 09:31:28 +0800

    封装axios

---
+++ src/api/request.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosRequestConfig } from "axios";
+import qs from "qs";
+
+axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.headers.post["Content-Type"] =
+  "application/x-www-form-urlencoded";
+
+export default function request(
+  url: string,
+  method: string = "get",
+  data = {}
+): Promise<any> {
+  return new Promise((resolve, reject) => {
+    let option: AxiosRequestConfig = {
+      url,
+      method,
+      params: data,
+      data: qs.stringify(data),
+      withCredentials: true,
+    };
+
+    if (method.toLowerCase() === "get") {
+      delete option.data;
+    } else if (method.toLowerCase() === "post") {
+      delete option.params;
+    }
+
+    axios(option)
+      .then((res) => {
+        console.log(res.data, "from request.ts line 30");
+
+        resolve(res.data);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
+}
